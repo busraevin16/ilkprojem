2b17e4c8f9fec51e2352a6e3b10b8005dbd87048
busraevin16 <bsra_evn52@hotmail.com>
2024-01-29 15:57:47 +0300

    Site için HTML dosyasını değiştirdim.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('', views.post_list, name='post_list'),
 ]
